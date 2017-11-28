e395f73a2517c0a0035f72129fa209bf7e770ead
suphansa <suphansa.23072538@gmail.com>
2017-11-28 09:37:01 +0700

    TestCondition

---
+++ src/oop_lab3/Tastcondition.java
@@ -0,0 +1,41 @@
+package oop_lab3;
+
+public class Tastcondition {
+    public static void main(String[] args) {
+        //IF
+
+        int x = 10, y = 20;
+        if (x > y || x + 15 > y)
+            System.out.println("x more than y");
+        System.out.println("oop");
+        //   If-else
+        if (x > y)
+
+        {
+            System.out.println("x more than y");
+        } else
+
+        {
+            System.out.println("x less than y");
+        }
+
+        //  IF - else -IF
+        if (x < 10) {
+            System.out.println("x less than 10");
+        } else if (x > 10) {
+            System.out.println("x more than 10");
+        } else {
+
+            System.out.println("x is 10");
+        }
+
+    }//main
+}//class
+
+
+
+
+
+
+
+
