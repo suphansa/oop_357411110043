08fcf11e34d018229f185279871783c8fe8457a3
suphansa <suphansa.23072538@gmail.com>
2018-01-23 11:42:37 +0700

    Class and object : MySuperCarApp

--- src/oop_lab7/MySuperCar.java
+++ src/oop_lab7/MySuperCar.java
@@ -1,4 +1,33 @@
 package oop_lab7;
 
+import java.util.Scanner;
+
 public class MySuperCar {
-}
+    public static void main (String[] args) {
+
+
+        SuperCar superCar1 = new SuperCar();
+        superCar1 = inputdata(superCar1);
+        System.out.println(superCar1.toString());
+
+
+
+
+    }//main
+    private static SuperCar inputdata(SuperCar s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your atudent data");
+        System.out.print("CarBand :");
+        s.setCarBrand(scanner.nextLine());
+        System.out.print("CarColor :");
+        s.setCarColor(scanner.nextLine());
+        System.out.print("CarEngineSize :");
+        s.setCarEngineSize(scanner.nextLine());
+        System.out.print("MaxSpeed :");
+        s.setMaxSpeed(scanner.nextLine());
+        System.out.print("CountryOfOrigin :");
+        s.setCountryOfOrigin(scanner.nextLine());
+
+        return s;
+    }
+}//class
